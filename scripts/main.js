d3d704219c164d4f96a3b3a08b53179185ea94f9
Satyn-Morris <satynleedesigns@gmail.com>
2019-12-01 12:23:50 -0600

    Final JS changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 var members ["Dave Grohl", "Nate Mendel", "Pat Smear", "Taylor Hawkins", "Rami Jaffee", "Chris Shiflett"]
 
 var tour = {city:["Chicago", "Seattle", "Edmonton"], date:["July 29th & 30th, 2018","September 1st, 2018","September 4th, 2018"]};
+
+var text = "";
+var x = 0;
+while (x < 5) {
+  text += "<br>Times running through the loop " + x;
+  x++;
+}
