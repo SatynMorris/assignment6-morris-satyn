9674b95f144acfde6de87f2aed66da067f8f15ba
Satyn-Morris <satynleedesigns@gmail.com>
2019-12-01 12:23:11 -0600

    Main JS changes

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+var members ["Dave Grohl", "Nate Mendel", "Pat Smear", "Taylor Hawkins", "Rami Jaffee", "Chris Shiflett"]
+
+var tour = {city:["Chicago", "Seattle", "Edmonton"], date:["July 29th & 30th, 2018","September 1st, 2018","September 4th, 2018"]};
